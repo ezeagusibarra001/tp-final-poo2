600ed43d3472d18ce4adb201297cb10cc375a44c
Tomás Aguirre <tomi-aguirre@hotmail.com>
2024-11-07 02:34:49 -0300

    feat: Implement rate after checkout

--- SistemaAlquileresTemporales/src/user/Tenant.java
+++ SistemaAlquileresTemporales/src/user/Tenant.java
@@ -1,23 +1,23 @@
 package user;
 
+import java.util.Date;
+
 import property.Property;
 import ranking.Ranking;
+import site.Category;
 import site.Site;
 
 public class Tenant extends User {
-	private Ranking ranking;
-
 	public Tenant(String fullName, String email, int phone) {
 		super(fullName, email, phone);
-		this.ranking = new Ranking();
+	}
+	
+	@Override
+	public void additionalRatings(Property property, Ranking ranking) {
+		property.getRanking().addScores(ranking);
 	}
 
-	public void requestBooking(Site s, Property p) {
-		s.requestBooking(this, p);
-	}
-
-	// Getters y Setters
-	public Ranking getRanking() { // lo necesito?
-		return this.ranking;
+	public void requestBooking(Site site, Property property, Date checkInDate, Date checkOutDate) {
+		site.requestBooking(this, property, checkInDate, checkOutDate);
 	}
 }
--- SistemaAlquileresTemporales/src/user/User.java
+++ SistemaAlquileresTemporales/src/user/User.java
@@ -1,16 +1,48 @@
 package user;
 
+import site.Category;
+import ranking.Ranking;
+import property.Property;
+
 public abstract class User {
 	// pueden cambiar a protected
 	private String fullName;
 	private String email;
 	private int phone;
+	private Ranking ranking;
 	
 	public User(String fullName, String email, int phone) {
-		this.fullName = fullName;
-		this.email = email;
-		this.phone = phone;
+		this.setFullName(fullName);
+		this.setEmail(email);
+		this.setPhone(phone);
+		this.ranking = new Ranking();
 	}
 	
-	// getters y setters...?
+	public final void rateAfterCheckout(User otherUser, Property property, Ranking rankingUser, Ranking rankingProperty) {
+		this.rateUser(otherUser, rankingUser);
+		this.additionalRatings(property, rankingProperty);
+	}
+	
+	private void rateUser(User user, Ranking ranking) {
+		user.getRanking().addScores(ranking);
+	}
+	
+	protected void additionalRatings(Property property, Ranking ranking) {}
+
+	// Getters
+    public Ranking getRanking() {
+    	return this.ranking;
+    }
+	// Setters
+	private void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	private void setEmail(String email) {
+		this.email = email;
+	}
+	
+	private void setPhone(int phone) {
+		this.phone = phone;
+	}
 }
